f155158159678b3d5b6d4f2b2967da480d4f0719
John Doe <johndoe@email.com>
2024-10-19 07:38:30 +0500

    asdgasdgf

--- test.py
+++ test.py
@@ -1,16 +1,31 @@
+import io
+import json
+
+import numpy as np
 import requests
+import soundfile as sf
+
+
+def preprocess_audio(file_path):
+    with open(file_path, "rb") as audio_file:
+        audio_data, samplerate = sf.read(audio_file)
+        if len(audio_data.shape) > 1:
+            audio_data = audio_data.mean(axis=1)
+        audio_data = np.array(audio_data, dtype=np.float32)
+        return audio_data.tolist(), samplerate
 
 
 def test_transcribe_audio(file_path):
-    with open(file_path, "rb") as audio_file:
-        files = {"file": (file_path, audio_file, "audio/wav")}
-        url = "https://peoples-models.onrender.com/transcribe"
-        response = requests.post(url, files=files)
-        if response.status_code == 200:
-            result = response.json()
-            print("Transcription result:", result)
-        else:
-            print(f"Failed to transcribe audio. Status code: {response.status_code}")
+    audio_data, samplerate = preprocess_audio(file_path)
+    payload = {"audio_data": audio_data, "samplerate": samplerate}
+    url = "https://peoples-models.onrender.com/transcribe"
+    response = requests.post(url, json=payload)
+
+    if response.status_code == 200:
+        result = response.json()
+        print("Transcription result:", result)
+    else:
+        print(f"Failed to transcribe audio. Status code: {response.status_code}")
 
 
 if __name__ == "__main__":
